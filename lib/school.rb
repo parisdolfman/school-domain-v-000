00a5fb91499d35a99fb6351357c782e873df46c7
Paris Dolfman <parisdolfman@gmail.com>
2018-07-26 19:19:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,18 +3,15 @@
 class School
   def initialize(name)
     @name = name
+  end
+
+  def roster
     roster = {}
   end
 
-   
-
-  def roster=(roster)
-    @roster = roster
+  def add_student(name, grade)
+    roster[name] = []
+    roster[name] << grade
   end
-
-  #def add_student(name, grade)
-  #  roster[name] = []
-  #  roster[name] << grade
-  #end
 
 end
