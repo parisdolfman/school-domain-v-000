2c96d29106156fa0776b55642eb32fea57973d4b
Paris Dolfman <parisdolfman@gmail.com>
2018-07-26 19:38:39 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,6 +13,7 @@ class School
   def add_student(name, grade)
     @student_name = name
     @grade = grade
+    if @roster.include?(grade) == false
     @roster[grade] = []
   end
     @roster[grade] << name
